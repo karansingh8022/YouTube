f38ff002080d2adfa0d392451fca408718c967cd
Karan Singh <karansinghbrh8@gmail.com>
2024-07-12 18:59:07 +0530

    created apiError apiResponse asyncHandler so that we can give formatted error and response

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err)=>{
+        next(err)
+    })
+}
+
+export {asyncHandler}
+
+
+
+
+
+//***************one way of handling async function************** */
+//this is done by declaring higher order function and creating a wrapper function that takes that function 
+//which needs to be handled asynchronously and it get executed
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {}
+
+// const asynHandler  = (func) => async (err, req, res, next) => {
+//     try {
+//         await func(err, req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+
