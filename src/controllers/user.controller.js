5d97182e59d444f7d5865550150a8ed236bea313
Karan Singh <karansinghbrh8@gmail.com>
2024-07-14 19:16:27 +0530

    added getUserChannelProfile controller, learned about aggregate pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -267,12 +267,194 @@ const changeCurrentPassword = asyncHandler( async(req, res) => {
 
 
 
+const getCurrentUser = asyncHandler( async(req, res) => {
+    //we will user middleware to get the user verifyJWT
+    return res
+    .status(200)
+    .json( new ApiResponse(200, req.user, "getCurrentUser!!! current user fetched successfully") )
+} )
+
+
+
+
+const updateAccountDetails = asyncHandler( async (req, res) => {
+    //verify using middleware that user is authorized for the change or not
+    const { fullName, email } = req.body;
+
+    if(!fullName || !email) { throw new ApiError(400, "updateAccountDetails!!! all the fields are required") }
+
+    //you can user req.user.fullName = fullName: user.save() as well but as here we have multiple value so we did this way
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email, //if both field have same name they can be updated this way as well
+            }
+        },
+        { new: true } //this is done to return the updated value
+    ).select(" -password -refreshToken ") 
+    
+    return res
+    .status(200)
+    .json( new ApiResponse(200, user, "updateAccountDetails!!! Account details updated successfully") )
+} )
+
+
+
+const updateUserAvatar = asyncHandler( async (req, res) => {
+    //we will be using multer middleware to update files and verifyJWT to verify the user
+    
+    // console.log(req.file);
+    //**get the path */
+    const avatarLocalPath = req.file?.path;
+
+    if(!avatarLocalPath) { throw new ApiError(401, "updateUserAvatar!!! file path is not found") }
+
+    //**upload on cloudinary */
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if(!avatar.url) { throw new ApiError(401, "updateUserAvatar!!! failed to upload avatar") }
+
+    //**update the user */
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url,
+            }
+        },
+        { new: true }
+    ).select(" -password -refreshToken ")
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, user, "updateUserAvatar!!! Avatar updated successfully") )
+    
+} )
+
+
+
+const updateUserCoverImage = asyncHandler( async (req, res) => {
+    //we will be using multer middleware to update files and verifyJWT to verify the user
+    
+    // console.log(req.file);
+    //**get the path */
+    const coverImageLocalPath = req.file?.path;
+
+    if(!coverImageLocalPath) { throw new ApiError(401, "updateUserCoverImage!!! file path is not found") }
+
+    //**upload on cloudinary */
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!coverImage.url) { throw new ApiError(401, "updateUserCoverImage!!! failed to upload Cover Image") }
+
+    //**update the user */
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url,
+            }
+        },
+        { new: true }
+    ).select(" -password -refreshToken ")
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, user, "updateUserCoverImage!!! Cover Image updated successfully") )
+    
+} )
+
+
+
+
+const getUserChannelProfile = asyncHandler( async (req, res) => {
+    //**take the username from route */
+    const { username } = req.params;
+
+    if(!username?.trim()) { throw new ApiError(400, "getUserChannelProfile!! username is missing") }
+
+    //**apply aggragation pipeline  */
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            //to get all the subscribers
+            $lookup: {
+                from: "subscriptions", //take the model from where you want to lookup, take the name how it is stored in mongodb
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            //to get all the channels to whom we have subscribed to
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers",
+                },
+                channelsSubscribedTo: {
+                    $size: "$subscribedTo",
+                },
+                isSubscribed: {//checking if the user is subscribed or not to display the subscribe button
+                    $cond: {
+                        if: { $in: [ req.user?._id, "$subscribers.subscriber" ] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            //the data you want to display or give the selected fields
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                subscriberCount: 1,
+                channelsSubscribedTo: 1,
+                avatar: 1,
+                coverImage: 1,
+                isSubscribed: 1
+            }
+        }
+    ])
+
+
+    // console.log(channel);
+
+    if(!channel?.length){ throw new ApiError(401, "getUserChannelProfile!!! Channel does not exists") }
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, channel[0], "getUserChannelProfile!!! user channel fetched successfully"))
+} )
+
+
+
 export { 
     registerUser, 
     loginUser, 
     logoutUser, 
     refreshAccessToken,
-    changeCurrentPassword
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile
 };
 
 
@@ -284,3 +466,7 @@ export {
 //         message: "karan singh",
 //     })
 // } )
+
+
+
+//NOTE: read about aggregation pipelines
